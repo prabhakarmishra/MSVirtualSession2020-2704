76af8db704c6832903600ecae0debe7ac2fbba74
w2cluster99@iiht.tech <w2cluster99@iiht.tech>
2020-04-28 14:31:58 +0530

    CSL

---
+++ lab-5/word-server/src/main/java/demo/WordController.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WordController {
+
+	@Value("${words}") String words;
+	
+	@GetMapping("/")
+	public @ResponseBody String getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i]+" "+System.getProperty("catalina.base");
+	}
+}
